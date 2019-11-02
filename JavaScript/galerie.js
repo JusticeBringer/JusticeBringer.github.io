bd40644f9df714c8720a84cc64d5e111f8829da8
JusticeBringer <gabithebigg@gmail.com>
2019-11-02 15:46:51 +0200

    Galery - updated

--- JavaScript/galerie.js
+++ JavaScript/galerie.js
@@ -2,44 +2,58 @@ function myFunctionBuc() {
     var x = document.getElementById("Ias");
     x.style.display = "none";
 
-    var x = document.getElementById("Buc");
-    if (x.style.display === "none") {
-        x.style.display = "grid";
+    x = document.getElementById("Brv");
+    x.style.display = "none";
 
-    } else {
+    x = document.getElementById("Tul");
+    x.style.display = "none";
+
+    x = document.getElementById("Buc");
+    if (x.style.display === "none")
         x.style.display = "grid";
-    }
 }
 
 function myFunctionIas() {
     var x = document.getElementById("Buc");
     x.style.display = "none";
 
-    var x = document.getElementById("Ias");
-    if (x.style.display === "none") {
-        x.style.display = "grid";
+    x = document.getElementById("Brv");
+    x.style.display = "none";
 
-    } else {
+    x = document.getElementById("Tul");
+    x.style.display = "none";
+
+    x = document.getElementById("Ias");
+    if (x.style.display === "none")
         x.style.display = "grid";
-    }
 }
 
 function myFunctionBrv() {
-    var x = document.getElementById("Brv");
-    if (x.style.display === "none") {
-        x.style.display = "grid";
+    var x = document.getElementById("Buc");
+    x.style.display = "none";
 
-    } else {
-        x.style.display = "none";
-    }
+    x = document.getElementById("Ias");
+    x.style.display = "none";
+
+    x = document.getElementById("Tul");
+    x.style.display = "none";
+
+    x = document.getElementById("Brv");
+    if (x.style.display === "none")
+        x.style.display = "grid";
 }
 
 function myFunctionTul() {
-    var x = document.getElementById("Tul");
-    if (x.style.display === "none") {
-        x.style.display = "grid";
+    var x = document.getElementById("Ias");
+    x.style.display = "none";
 
-    } else {
-        x.style.display = "none";
-    }
+    x = document.getElementById("Brv");
+    x.style.display = "none";
+
+    x = document.getElementById("Buc");
+    x.style.display = "none";
+
+    x = document.getElementById("Tul");
+    if (x.style.display === "none")
+        x.style.display = "grid";
 }
