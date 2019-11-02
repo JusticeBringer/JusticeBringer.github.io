4422f5c6274aae6518147647d0d3db22dd7995ba
JusticeBringer <gabithebigg@gmail.com>
2019-11-02 15:14:26 +0200

    Folders + Separate files

---
+++ JavaScript/galerie.js
@@ -0,0 +1,45 @@
+function myFunctionBuc() {
+    var x = document.getElementById("Ias");
+    x.style.display = "none";
+
+    var x = document.getElementById("Buc");
+    if (x.style.display === "none") {
+        x.style.display = "grid";
+
+    } else {
+        x.style.display = "grid";
+    }
+}
+
+function myFunctionIas() {
+    var x = document.getElementById("Buc");
+    x.style.display = "none";
+
+    var x = document.getElementById("Ias");
+    if (x.style.display === "none") {
+        x.style.display = "grid";
+
+    } else {
+        x.style.display = "grid";
+    }
+}
+
+function myFunctionBrv() {
+    var x = document.getElementById("Brv");
+    if (x.style.display === "none") {
+        x.style.display = "grid";
+
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function myFunctionTul() {
+    var x = document.getElementById("Tul");
+    if (x.style.display === "none") {
+        x.style.display = "grid";
+
+    } else {
+        x.style.display = "none";
+    }
+}
