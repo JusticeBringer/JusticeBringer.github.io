96ba16463f9902b3509c8d06d507fabebe75038b
JusticeBringer <gabithebigg@gmail.com>
2019-11-02 17:38:47 +0200

    More content

--- JavaScript/galerie.js
+++ JavaScript/galerie.js
@@ -1,4 +1,13 @@
 function myFunctionBuc() {
+    var y = document.getElementById("IasInfo");
+    y.style.display = "none";
+
+    y = document.getElementById("BrvInfo");
+    y.style.display = "none";
+
+    y = document.getElementById("TulInfo");
+    y.style.display = "none";
+
     var x = document.getElementById("Ias");
     x.style.display = "none";
 
@@ -9,11 +18,26 @@ function myFunctionBuc() {
     x.style.display = "none";
 
     x = document.getElementById("Buc");
+    y = document.getElementById("BucInfo");
     if (x.style.display === "none")
         x.style.display = "grid";
+    if (y.style.display === "none")
+        y.style.display = "block";
 }
 
 function myFunctionIas() {
+    var y = document.getElementById("BucInfo");
+    y.style.display = "none";
+
+    y = document.getElementById("BrvInfo");
+    y.style.display = "none";
+
+    y = document.getElementById("TulInfo");
+    y.style.display = "none";
+
+    var y = document.getElementById("BucInfo");
+    y.style.display = "none";
+
     var x = document.getElementById("Buc");
     x.style.display = "none";
 
@@ -24,11 +48,26 @@ function myFunctionIas() {
     x.style.display = "none";
 
     x = document.getElementById("Ias");
+    y = document.getElementById("IasInfo");
     if (x.style.display === "none")
         x.style.display = "grid";
+    if (y.style.display === "none")
+        y.style.display = "block";
 }
 
 function myFunctionBrv() {
+    var y = document.getElementById("BucInfo");
+    y.style.display = "none";
+
+    y = document.getElementById("IasInfo");
+    y.style.display = "none";
+
+    y = document.getElementById("TulInfo");
+    y.style.display = "none";
+
+    var y = document.getElementById("BucInfo");
+    y.style.display = "none";
+
     var x = document.getElementById("Buc");
     x.style.display = "none";
 
@@ -39,11 +78,26 @@ function myFunctionBrv() {
     x.style.display = "none";
 
     x = document.getElementById("Brv");
+    y = document.getElementById("BrvInfo");
     if (x.style.display === "none")
         x.style.display = "grid";
+    if (y.style.display === "none")
+        y.style.display = "block";
 }
 
 function myFunctionTul() {
+    var y = document.getElementById("BucInfo");
+    y.style.display = "none";
+
+    y = document.getElementById("BrvInfo");
+    y.style.display = "none";
+
+    y = document.getElementById("IasInfo");
+    y.style.display = "none";
+
+    var y = document.getElementById("BucInfo");
+    y.style.display = "none";
+
     var x = document.getElementById("Ias");
     x.style.display = "none";
 
@@ -54,6 +108,9 @@ function myFunctionTul() {
     x.style.display = "none";
 
     x = document.getElementById("Tul");
+    y = document.getElementById("TulInfo");
     if (x.style.display === "none")
         x.style.display = "grid";
+    if (y.style.display === "none")
+        y.style.display = "block";
 }
